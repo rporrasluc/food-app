418ecc90ec32e053fc69cf5deb343ac49079f7bd
Rafael Porras <rafael.porras@shopify.com>
2021-12-27 17:30:13 +0100

    Understanding GraphQL for Beginners

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,34 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :all_food, [Types::FoodType], null: false, description: 'Get all the food items'
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def all_food
+      Food.all
+    end
+
+    field :nutrition, [Types::NutritionType], null: false, description: 'Get all the nutrition items'
+
+    def nutrition
+      Nutrition.all
+    end
+
+    field :food, Types::FoodType, null: false do
+      description 'Get a food item based on id.'
+      argument :id, ID, required: true
+    end
+
+    def food(id:)
+      Food.find(id)
+    end
+
+    field :find_food, [Types::FoodType], null: false do
+      description 'Get all food items based on place of origin.'
+      argument :place_of_origin, String, required: true
+    end
+
+    def find_food(place_of_origin:)
+      Food.where(place_of_origin: place_of_origin)
     end
   end
 end
